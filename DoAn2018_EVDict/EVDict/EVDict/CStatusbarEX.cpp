9324ce7c9e4833d414f784815a6f96baff855972
TrinhVanHieu <37131030+TrinhVanHieu@users.noreply.github.com>
2018-09-24 21:01:40 +0700

    Update
    hiển thị lịch sử tìm kiếm trong combobox

--- DoAn2018_EVDict/EVDict/EVDict/CStatusbarEX.cpp
+++ DoAn2018_EVDict/EVDict/EVDict/CStatusbarEX.cpp
@@ -15,7 +15,7 @@ HWND CStatusbarEX::CreateMainFrame(HWND hWnd)
 	HWND hStatus = CreateWindowEx(0, STATUSCLASSNAME, NULL, WS_CHILD | WS_VISIBLE |
 		SBARS_SIZEGRIP, 0, 0, 0, 0, hWnd, (HMENU)IDC_MAIN_STATUS, GetModuleHandle(NULL), NULL);
 
-	int statwidths[] = { 150, -1 };
+	int statwidths[] = { 200, -1 };
 	SendMessage(hStatus, SB_SETPARTS, sizeof(statwidths) / sizeof(int), (LPARAM)statwidths);
 	SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)_T("Eng - Viet Dictionary"));
 	return hStatus;

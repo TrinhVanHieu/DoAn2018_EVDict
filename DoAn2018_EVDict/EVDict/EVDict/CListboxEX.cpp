5f39eade3016eb98a2fdaba3a5c6f04aa464e594
TrinhVanHieu <37131030+TrinhVanHieu@users.noreply.github.com>
2018-12-31 17:23:49 +0700

    update
    word + pdf + slide

--- DoAn2018_EVDict/EVDict/EVDict/CListboxEX.cpp
+++ DoAn2018_EVDict/EVDict/EVDict/CListboxEX.cpp
@@ -14,5 +14,6 @@ HWND CListboxEX::CreateMainFrame(HWND hWnd)
 {
 	HWND hListbox = CreateWindowEx(WS_EX_CLIENTEDGE, _T("listbox"), _T(""), WS_CHILD | WS_VISIBLE | 
 		WS_VSCROLL |  LBS_NOTIFY, 0, 55, 200, 100, hWnd, (HMENU)IDC_MAIN_LISTBOX, GetModuleHandle(NULL), NULL);
+
 	return hListbox;
 }
